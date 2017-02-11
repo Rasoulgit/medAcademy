69c0855e5d3b989485d4731bfe0850b55ea5519d
Rasoulgit <khosravi_rasoul@yahoo.com>
2017-02-11 22:22:33 +0200

    initializeing

---
+++ HECS/Controllers/PagesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HECS.Controllers
+{
+    public class PagesController : Controller
+    {
+        //
+        // GET: /Pages/
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Consultation()
+        {
+            return View();
+        }
+        public ActionResult Enrollment()
+        {
+            return View();
+        }
+        public ActionResult Transfer()
+        {
+            return View();
+        }
+        public ActionResult Accomodation()
+        {
+            return View();
+        }
+        public ActionResult Translation_Legalization()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Partners()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult OpenDays()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ HECS/Controllers/UniversityController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HECS.Controllers
+{
+    public class UniversityController : Controller
+    {
+        //
+        // GET: /University/
+
+        public ActionResult Index(string Id)
+        {
+            ViewBag.SubPage = Id;
+            return View();
+        }
+        public ActionResult Medical_University_of_Sofia()
+        {
+            return PartialView("_Medical_University_of_Sofia");
+        }
+        public ActionResult Sofia_University_St_Kliment_Ohridski()
+        {
+            return PartialView("_Sofia_University_St_Kliment_Ohridski");
+        }
+        public ActionResult UNIVERSITY_OF_FORESTRY()
+        {
+            return PartialView("_UNIVERSITY_OF_FORESTRY");
+        }
+        public ActionResult University_of_Architecture()
+        {
+            return PartialView("_University_of_Architecture");
+        }
+        public ActionResult MEDICINA_YNIVERSITET_PLOVDIV()
+        {
+            return PartialView("_MEDICINA_YNIVERSITET_PLOVDIV");
+        }
+        public ActionResult Plovdiv_University_Paisii_Hilendarski()
+        {
+            return PartialView("_Plovdiv_University_Paisii_Hilendarski");
+        }
+        public ActionResult Medical_University_of_Varna_ProfDrParaskevStoyanov()
+        {
+            return PartialView("_Medical_University_of_Varna_ProfDrParaskevStoyanov");
+        }
+
+        public ActionResult National_Sports_Academy()
+        {
+            return PartialView("_National_Sports_Academy");
+        }
+        public ActionResult Technical_University_of_Sofia()
+        {
+            return PartialView("_Technical_University_of_Sofia");
+        }
+
+        public ActionResult NursingSofia()
+        {
+            return PartialView("_NursingSofia");
+        }
+
+        public ActionResult Veterinery()
+        {
+            return PartialView("_Veterinery");
+        }
+
+        public ActionResult TrakiaUniversity()
+        {
+            return PartialView("_TrakiaUniversity");
+        }
+
+        public ActionResult Lasi()
+        {
+            return PartialView("_Lasi");
+        }
+
+        public ActionResult GrPopa()
+        {
+            return PartialView("_GrPopa");
+        }
+
+        public ActionResult Ovidius()
+        {
+            return PartialView("_Ovidius");
+        }
+
+    }
+}
